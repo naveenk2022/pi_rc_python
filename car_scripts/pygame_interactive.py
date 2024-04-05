8c9992cae012af0426b6f62d99abf72c6f05cffe
naveenk2022 <naveen.kannan12@gmail.com>
2024-04-05 10:06:51 -0400

    Edited the interactive pygame script

--- car_scripts/pygame_interactive.py
+++ car_scripts/pygame_interactive.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 import pygame
 import sys
+import os
 # Defining the GPIO pin connections
 # Motor 1
 in1 = 22
@@ -25,7 +26,7 @@ GPIO.setup(ena,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 p=GPIO.PWM(ena,1000)
-p.start(25)
+p.start(50)
 
 # Setting up Motor 2
 GPIO.setup(in3,GPIO.OUT)
@@ -34,19 +35,58 @@ GPIO.setup(enb,GPIO.OUT)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 q=GPIO.PWM(enb,1000)
-q.start(25)
+q.start(50)
 
 
 # Initialize Pygame
-pygame.init()
+pygame.display.init()
+pygame.font.init() 
+# Set up the screen (you won't actually see it, but Pygame requires it)
+window = pygame.display.set_mode((640,480))
+pygame.display.set_caption("KushWagen v1.0")
 
-from pygame.locals import *
+# Set up colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-windowSurfaceObj = pygame.display.set_mode((100,100))
+# Set up font
+font = pygame.font.SysFont(None, 40)
+
+# Defining a text function
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Define font
+font = pygame.font.Font(None, 36)
+
+def render_text():
+    window.fill(WHITE)  # Fill the screen with white color
+
+    # Render text
+    text = [
+        "W - Move Forward",
+        "A - Turn Left",
+        "S - Turn Right",
+        "D - Reverse",
+        "Press H to increase speed!",
+        "Press M to decrease speed.",
+        "Press E to exit."
+    ]
+
+    # Display text on the screen
+    y_offset = 100
+    for line in text:
+        rendered_text = font.render(line, True, BLACK)
+        window.blit(rendered_text, (50, y_offset))
+        y_offset += 50
+
+    pygame.display.flip()  # Update the display
+
 
 # creating a running loop
 while True:
-       
+    render_text()
     # creating a loop to check events that 
     # are occurring
     for event in pygame.event.get():
@@ -100,6 +140,10 @@ while True:
                 p.ChangeDutyCycle(75)
                 q.ChangeDutyCycle(75)
 
+            # Key "M" - MEDIUM
+            if event.key == pygame.K_h:
+                p.ChangeDutyCycle(50)
+                q.ChangeDutyCycle(50)
 
         # Key RELEASED
         elif event.type == pygame.KEYUP:
