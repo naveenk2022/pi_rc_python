6f8438d2287e919b6c794cec2219074b1fa522fc
naveenk2022 <naveen.kannan12@gmail.com>
2024-04-04 20:51:14 -0400

    Edited the server TCP script

--- server_scripts/tcp_server_init.py
+++ server_scripts/tcp_server_init.py
@@ -1,5 +1,5 @@
 import socket
-
+import pygame
 tcp1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_ip = ""         # Any interface
 port = 9000        # Arbitrary non-privileged port
@@ -10,16 +10,43 @@ tcp1.bind((tcp_ip, port))
 tcp1.listen(1)
 con, addr = tcp1.accept()
 print("TCP Connection from:", addr)
+output=""
+# Initialize Pygame
+pygame.init()
+pygame.display.set_mode((300,300))
 
 while True:
-    user_input = input("Enter a response: ")
-    
-    if user_input.lower() == 'exit':
-        print("Closing connection...")
-        con.close()
-        tcp1.close()
-        break
-    
-    con.send(user_input.encode('utf-8'))
+    # creating a loop to check events that 
+    # are occurring
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # key PRESS
+        if event.type == pygame.KEYDOWN:
+
+            # Key "W"- FORWARD
+            if event.key == pygame.K_w:
+                output='w'
+            # Key "A" - LEFT
+            if event.key == pygame.K_a:
+                output='a'
+            # Key "S" - LEFT
+            if event.key == pygame.K_s:
+                output='s'
+            # Key "D" - REVERSE
+            if event.key == pygame.K_d:
+                output='d'
+            # Key "E" - EXIT
+            if event.key == pygame.K_e:
+                output='exit'
+                break
+            # Key "E" - EXIT
+            if event.key == pygame.K_h:
+                output='h'
+                break
+
+    con.send(output.encode('utf-8'))
 
 tcp1.close()
